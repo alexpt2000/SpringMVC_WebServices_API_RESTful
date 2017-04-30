6c313fa18bea24deae61a3e76667e70e06b23f74
alexpt2000gmit <alexpt2000@192.168.1.51>
2017-04-30 21:56:02 +0100

    2.3 Criando uma representação para o recurso Livro

--- SocialBooksAPI/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ SocialBooksAPI/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -1,16 +1,28 @@
 package com.algaworks.socialbooks.resources;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.algaworks.socialbooks.domain.Livro;
+
+@RestController // RestController e a notacao para Rest enquanto Controller e
+				// apenas para MVC
 public class LivrosResources {
-	
+
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
-	public String listar(){
-		
-		return "Rest Aplicado, Git passo-a-passo";
+	public List<Livro> listar() {
+
+		Livro l1 = new Livro("Rest Aplicado");
+		Livro l2 = new Livro("Git passo-a-passo");
+
+		Livro[] livros = { l1, l2 }; // Vector - Conectando mais de uma lista
+
+		return Arrays.asList(livros);
 	}
 
 }
