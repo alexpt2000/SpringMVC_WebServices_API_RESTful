e0a0e8ecb3d85b94121660b7489f1bcab6906993
alexpt2000gmit <alexpt2000@192.168.1.51>
2017-05-02 20:56:51 +0100

    2.7 Buscando um livro com o uso da anotação @PathVariable

--- SocialBooksAPI/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ SocialBooksAPI/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +33,12 @@ public class LivrosResources {
 
 		livrosRepository.save(livro);
 
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Livro Buscar(@PathVariable("id") Long id) {
+
+		return livrosRepository.findOne(id);
 	}
 
 }
